158a257a2aa3785a70bfa4191a6754eb007919e7
Dmitriy Ramus <ramusdmitry@gmail.com>
2022-06-18 21:41:16 +0300

    Eighth attempt

--- files/make.sh
+++ files/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xelatex -interaction=nonstopmode -halt-on-error main.tex
 # cp files/main.pdf cv.pdf
-git config --gloabl user.email "ramusdmitry@gmail.com"
+git config --global user.email "ramusdmitry@gmail.com"
 git config --global user.name "Dmitry Ramus"
