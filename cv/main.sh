143294e10ba944186691fe7bb4a6d46018c54b30
Dmitry Ramus <ramusdmitry@gmail.com>
2022-06-19 14:01:19 +0300

    42th commit

--- cv/main.sh
+++ cv/main.sh
@@ -2,6 +2,6 @@ cd cv
 xelatex -interaction=nonstopmode -halt-on-error main.tex
 cd ..
 
-cp cv/main.pdf cv.pdf
+cp cv/main.pdf ./cv.pdf
 git config --global user.email "ramusdmitry@gmail.com"
 git config --global user.name "Dmitry Ramus"
